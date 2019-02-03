ac32f5700e3bf7bb09755e77d41897f3f55d778d
Joel Söderman <joelhsoderman@gmail.com>
2019-02-03 16:21:33 +0100

    cli barebones finished

--- App/Program.fs
+++ App/Program.fs
@@ -7,16 +7,34 @@ open System
 open Extensions
 open RunProcess
 open CommandLine
-open CommandLine.Text
+
+
+
+// Now, we are using CommandLineParser.FSharp. The current implementation
+// is almost good. The main issues are:
+//
+//    * On the output, the application is referred to as "App" instead of
+//      YACHT. The name seems to be inherited from project name, but maybe
+//      there is a way to work around it (probably).
+//
+//    * I have not yet figured out how to change the version number.
+//
+// A possible solution is to just use Argu. It worked fine - it was simple and
+// configurable. However, it does not currently work, because of some weird
+// error. Basically, it throws an exception because it does something illegal
+// with an array of System.String []. Since I did not want to spend too much
+// time on that, I just switched to CommandLineParser.FSharp. However, the best
+// long time solution would probably be to fix that error and use Argu. The
+// old code using Argu can be found below main.
 
 
 
 type Options =
   {
-    [<Option('n', "num", Default = "100"
+    [<Option('n', "num", Default = 100
             , HelpText = "The number of tests to run."
     )>]
-    num : string
+    num : int
   ;
     [<Option('c', "cmd"
             , Required = true
@@ -38,8 +56,95 @@ type Options =
 let main argv =
   let result = CommandLine.Parser.Default.ParseArguments<Options>(argv)
   match result with
-  | :? Parsed<Options> as parsed -> printfn "%A" parsed
+  | :? Parsed<Options> as parsed ->
+      let ss   = Array.ofSeq parsed.Value.cmd
+      let cmd  = ss.[0]
+      let args = ss.[1 ..]
+      let num  = parsed.Value.num
+
+      TestInfo.parserTestAll num
+      |> App.runTest {Cmd.cmd = cmd; Cmd.args = args}
+
   | :? NotParsed<Options> -> ()
   | _ -> ()
 
   0
+
+
+
+// open Argu
+
+
+
+// type CliArgs =
+//     |              Tests of int:int
+//     | [<Mandatory>]Cmd   of string:string
+//   with
+//     interface IArgParserTemplate with
+//       member s.Usage =
+//         match s with
+//         | Tests _ ->
+//             "specify how many tests to run. this is an optional parameter. \
+//              the default value is 100."
+//         | Cmd _ ->
+//             String.concat ""
+//               [| "specify the command to run. \n"
+//                ; "examples:\n"
+//                ; "  \"dotnet mylexer/mylexer.dll\"\n"
+//                ; "  \"mono mylexer.exe\"\n"
+//                ; "  \"mylexer.exe\"\n"
+//               |]
+
+
+
+// let getCmdAndNum cliArgs =
+
+//   let cmd, args =
+//     List.pick (function Cmd ss -> Some ss | _ -> None) cliArgs
+//     |> function
+//        | [||] -> ""    , [||]
+//        | ss   -> ss.[0], ss.[1 ..]
+
+//   let num =
+//     List.tryPick (function Num n -> Some n | _ -> None) cliArgs
+//     |> function
+//        | Some n -> n
+//        | None   -> 100
+
+//   { cmd = cmd; args = args }, TestInfo.parserTestAll num
+
+
+
+// let checkStructure =
+// #if INTERACTIVE || DEBUG
+//     true
+// #else
+//     false
+// #endif
+
+
+
+// let parser =
+//   let colorizer =
+//     function
+//     | ErrorCode.HelpText -> None
+//     | _ -> Some ConsoleColor.Red
+
+//   ArgumentParser.Create<CliArgs> (
+//     programName    = "yacht",
+//     checkStructure = checkStructure,
+//     errorHandler   = ProcessExiter(colorizer = colorizer))
+
+
+
+// [<EntryPoint>]
+// let main _ =
+//   let info =
+//     parser.Parse().GetAllResults()
+//     |> getCmdAndNum
+
+//   match Application.run info with
+//   | Error err -> Error.print err; 1
+//   | Ok      _ -> 0
+
+
